df1198c34587f61f9f13722f5615eb80694a68f3
Thomas Hofmann <thomas.hofmann@h-da.de>
2026-06-26 13:25:16 +0200

    wetter

--- database/seeders/WeatherSeeder.php
+++ database/seeders/WeatherSeeder.php
@@ -9,18 +9,17 @@ class WeatherSeeder extends Seeder
 {
     public function run(): void
     {
-        Weather::create([
-            'city' => 'Berlin',
-            'temperature' => 22.5,
-            'humidity' => 60,
-            'condition' => 'sunny',
-        ]);
+        $weatherRecords = [
+            ['city' => 'Berlin', 'temperature' => 22.5, 'humidity' => 60, 'condition' => 'sonnig'],
+            ['city' => 'Hamburg', 'temperature' => 18.2, 'humidity' => 72, 'condition' => 'bewölkt'],
+            ['city' => 'München', 'temperature' => 20.8, 'humidity' => 58, 'condition' => 'leicht bewölkt'],
+            ['city' => 'Köln', 'temperature' => 21.3, 'humidity' => 64, 'condition' => 'regnerisch'],
+            ['city' => 'Frankfurt', 'temperature' => 23.1, 'humidity' => 55, 'condition' => 'sonnig'],
+            ['city' => 'Stuttgart', 'temperature' => 19.7, 'humidity' => 68, 'condition' => 'windig'],
+        ];
 
-        Weather::create([
-            'city' => 'Hamburg',
-            'temperature' => 18.2,
-            'humidity' => 72,
-            'condition' => 'cloudy',
-        ]);
+        foreach ($weatherRecords as $weather) {
+            Weather::create($weather);
+        }
     }
 }
